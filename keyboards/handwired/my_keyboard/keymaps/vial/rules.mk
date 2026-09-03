fb897eb17af542fb649801f9967bcfe87cf04ffe
logansnowlogan-beep <logansnowlogan@gmail.com>
2026-09-03 11:36:50 -0500

    Update rules.mk

--- keyboards/handwired/my_keyboard/keymaps/vial/rules.mk
+++ keyboards/handwired/my_keyboard/keymaps/vial/rules.mk
@@ -1,9 +1,11 @@
+VIA_ENABLE = yes
 VIAL_ENABLE = yes
 
 DEBOUNCE_TYPE = sym_defer_g
 
-# Disable unused QMK features so introspection does not expect
-# placeholder arrays/definitions in keymap.c.
+# Keep this first build minimal and avoid unused dynamic feature hooks.
 COMBO_ENABLE = no
 TAP_DANCE_ENABLE = no
 KEY_OVERRIDE_ENABLE = no
+REPEAT_KEY_ENABLE = no
+QMK_SETTINGS = no
