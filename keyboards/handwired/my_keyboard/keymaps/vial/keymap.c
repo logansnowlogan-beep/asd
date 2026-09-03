02a7d51e06cbbc69bfa40a2e948ac63ffd614509
logansnowlogan-beep <logansnowlogan@gmail.com>
2026-09-03 10:47:04 -0500

    Update keymap.c

--- keyboards/handwired/my_keyboard/keymaps/vial/keymap.c
+++ keyboards/handwired/my_keyboard/keymaps/vial/keymap.c
@@ -20,26 +20,29 @@
 //
 #include QMK_KEYBOARD_H
 
-
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    // ── Layer 0: Base (QWERTY starting point) ────────────────────
-    // Edit these keycodes directly, or use Vial to remap via GUI.
     [0] = LAYOUT(
-        // row 0
-        KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_NO, KC_U, KC_I, KC_O, KC_P, KC_A, KC_S,
-        // row 1
-        KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_NO, KC_L, KC_SCLN, KC_Z, KC_X, KC_C, KC_V,
-        // row 2
-        KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        // row 3
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_TRNS
+        /* Row 0: 12 keys (Cols: 0, 1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12) */
+        KC_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+        
+        /* Row 1: 12 keys (Cols: 0, 1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12) */
+        KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        
+        /* Row 2: 13 keys (Cols: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12) */
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_UP,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+        
+        /* Row 3: 8 keys  (Cols: 0, 1, 2, 4, 8, 9, 10, 12) */
+        KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  KC_LEFT, KC_DOWN, KC_RGHT, MO(1)
+    ),
+    [1] = LAYOUT(
+        /* Row 0: 12 keys */
+        KC_TRNS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
+        /* Row 1: 12 keys */
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
+        /* Row 2: 13 keys */
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        /* Row 3: 8 keys */
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     )
-
-    // ── To add a layer, copy the block above and change [0] to [1] ──
-    // Example:
-    // [1] = LAYOUT(
-    //     KC_TRNS, KC_TRNS, ...   // KC_TRNS = use layer below
-    // )
 };
 
