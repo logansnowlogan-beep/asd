1439f027319ef04e91dba83f5029b03999c49839
logansnowlogan-beep <logansnowlogan@gmail.com>
2026-09-03 11:28:12 -0500

    Add files via upload

---
+++ config.h
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x0B, 0x64, 0xDF, 0xD2, 0xBE, 0x0E, 0x2B, 0xAD}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 12 }
+
+#define DEBOUNCE 5
+
+#define DYNAMIC_KEYMAP_MACRO_COUNT 16
+#define DYNAMIC_KEYMAP_MACRO_EEPROM_SIZE 1024
