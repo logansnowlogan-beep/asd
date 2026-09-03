d00bf1f9daf617973a590a3e1fc7f43a1ed0226d
logansnowlogan-beep <logansnowlogan@gmail.com>
2026-09-03 11:29:06 -0500

    Delete rules (1).mk

--- rules (1).mk
+++
@@ -1,9 +0,0 @@
-VIAL_ENABLE = yes
-
-DEBOUNCE_TYPE = sym_defer_g
-
-# Disable unused QMK features so introspection does not expect
-# placeholder arrays/definitions in keymap.c.
-COMBO_ENABLE = no
-TAP_DANCE_ENABLE = no
-KEY_OVERRIDE_ENABLE = no
